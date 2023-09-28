3453cfadeaf6aae0d1b48ded149c71d5a1c52d79
alexbotha <alexbotha@hotmail.co.uk>
2023-09-28 10:19:00 +0100

    half finishd

--- lib/regex.py
+++ lib/regex.py
@@ -3,10 +3,10 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"[A-Z][']*([A-z][ \-']{0,1})+"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"[\d+]\S+\s+\d+\S+"
 phone_regex = re.compile(phone_number)
 
 email_address = r""
